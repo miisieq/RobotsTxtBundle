379884edff64560f0393188bdccda08a430221ea
Michał Szczech <michal.szczech@db-team.pl>
2017-10-06 08:28:25 +0200

    Removed deadcode

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('miisieq_robots_txt');
 
         return $treeBuilder;
     }
